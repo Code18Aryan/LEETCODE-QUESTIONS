ef49e9621ed313c11c2b3f0eea0d7e9643afac04
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-03-01 00:09:36 +0530

    Add files via upload

---
+++ GRAPH/787. Cheapest Flights Within K Stops.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+        unordered_map<int,vector<vector<int>>> adj;
+
+        for(auto x : flights){
+            
+            int u = x[0];
+            int v = x[1];
+            int wt = x[2];
+
+            adj[u].push_back({v,wt});
+        }
+
+        vector<int>distance(n,1e9);
+        distance[src] = 0;
+
+        queue<pair<int,pair<int,int>>>q;
+        q.push({0,{src,0}});
+
+        while(!q.empty()){
+
+            auto it = q.front();
+            q.pop();
+
+            int stops = it.first;
+            int node = it.second.first;
+            int dist = it.second.second;
+
+            if(stops > k) continue;
+
+            for(auto x : adj[node]){
+
+                int v = x[0];
+                int edwt = x[1];
+
+                if(dist + edwt < distance[v] && stops <= k){
+
+                    distance[v] = dist + edwt;
+                    q.push({stops + 1, {v,distance[v]}});
+                }
+            }
+        }
+
+        return distance[dst] == 1e9 ? -1 : distance[dst];
+    }
+};
