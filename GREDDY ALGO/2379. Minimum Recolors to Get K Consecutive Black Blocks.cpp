44dc05a88a1a5b248ac8a6d164b32531ab6f116c
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-03-10 23:43:06 +0530

    Add files via upload

---
+++ GREDDY ALGO/2379. Minimum Recolors to Get K Consecutive Black Blocks.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        
+        int cnt = 0;
+        int n = blocks.size();
+
+        int mini = 1e9;
+
+        int i = 0, j = 0;
+
+        while( j < n){
+            
+            if(blocks[j] == 'W') cnt++;
+
+            if(j-i+1 == k){
+
+                mini = min(mini,cnt);
+
+                if(blocks[i] == 'W'){
+
+                    cnt--;
+                }
+
+                i++;
+            }
+
+            j++;
+        }
+
+        return mini;
+    }
+};
