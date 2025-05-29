28aeb956aad42e2e6b4176c11eea7f79f01c8b61
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-05-29 23:58:39 +0530

    Add files via upload

---
+++ TREES/100. Sum of nodes on the longest path.cpp
@@ -0,0 +1,53 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+  
+    pair<int,int> solve(Node* root){
+        
+        if(root == NULL) return {0,0};
+        
+        pair<int,int> left = solve(root -> left);
+        pair<int,int> right = solve(root -> right);
+        
+        if(left.first > right.first){
+            
+            return {left.first + 1 , root -> data + left.second};
+        }
+        else if(left.first < right.first){
+            
+            return {right.first + 1, root -> data + right.second};
+        }
+        else{
+            
+            if(left.second > right.second){
+                
+                return {left.first + 1, root -> data + left.second};
+            }
+            else{
+                
+                return {right.first +1 , root -> data + right.second};
+            }
+        }
+    }
+  
+  
+    int sumOfLongRootToLeafPath(Node *root) {
+        
+        pair<int,int> ans = solve(root);
+        
+        return ans.second;
+    }
+};
