eec0df4ce257e9ae36119eabdd7b20fb070005a9
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-07-21 23:57:31 +0530

    Add files via upload

---
+++ LINKEDLIST/BASIC.cpp
@@ -0,0 +1,43 @@
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> LLTraversal(ListNode *head) {
+
+        if(head == NULL) return {};
+
+        vector<int> ans;
+
+        ListNode* temp = head;
+z
+        while(temp){
+
+            ans.push_back(temp -> val);
+            temp = temp -> next;
+        }
+
+        return ans;
+    }
+};
