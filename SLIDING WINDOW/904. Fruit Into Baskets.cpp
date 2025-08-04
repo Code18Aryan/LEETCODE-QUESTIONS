ba5ccc739fc689504daeabdaa2cc7d62e57b237e
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-08-04 14:45:46 +0530

    Add files via upload

---
+++ SLIDING WINDOW/904. Fruit Into Baskets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        
+        unordered_map<int,int> mpp;
+        int i=0, j=0, maxiFruit = 0;
+        int n = fruits.size();
+
+        while( j < n){
+
+            mpp[fruits[j]]++;
+
+            while(mpp.size() > 2){
+
+                mpp[fruits[i]]--;
+                
+                if(mpp[fruits[i]] == 0){
+
+                    mpp.erase(fruits[i]);
+                }
+
+                i++;
+            }
+
+            maxiFruit = max(maxiFruit,j-i+1);
+            j++;
+        }
+
+        return maxiFruit;
+    }
+};
