3f0aa27bea3c746121ad048b48567f543176efac
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-08-27 23:29:08 +0530

    Add files via upload

--- BINARY SEARCH/1283. Find the Smallest Divisor Given a Threshold.cpp
+++ BINARY SEARCH/1283. Find the Smallest Divisor Given a Threshold.cpp
@@ -1,41 +1,44 @@
 class Solution {
-  public:
-  
-    
-    int solve(vector<int>&nums, int mid){
-        
+
+private:
+
+  int findSmallestDivisor(int divisor, vector<int>&nums){
+
+        int n = nums.size();
+
         int sum = 0;
-        
-        for(int i=0; i<nums.size(); i++){
-            
-            sum += ceil((double)nums[i]/(double)mid);
+
+        for(int i=0; i<n; i++){
+
+            sum += ceil(double(nums[i])/ double(divisor));
         }
-        
+
         return sum;
-    }
-  
-  
-    int smallestDivisor(vector<int>& arr, int k) {
+  }
+
+
+public:
+  int smallestDivisor(vector<int> &nums, int limit) {
        
-        int low = 1;
-        int high = 1e6;
-        int ans  = -1;
-        
-        while(low <= high){
-            
-            int mid = low + (high - low)/2;
-            
-            int sum = solve(arr,mid);
-            
-            if(sum <= k){
-                
+       int low = 1;
+       int high = *max_element(nums.begin(),nums.end());
+       int ans = -1;
+
+       while(low <= high){
+
+            int mid = (low + high)/2;
+
+            int currentSamllestDivisor = findSmallestDivisor(mid,nums);
+
+            if(currentSamllestDivisor <= limit){
+
                 ans = mid;
                 high = mid-1;
             }
-            
+
             else low = mid+1;
-        }
-        
-        return ans;
+       }
+
+       return ans;
     }
 };
