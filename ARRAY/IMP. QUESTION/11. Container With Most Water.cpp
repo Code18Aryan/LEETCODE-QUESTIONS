d780a4538a19190a075d2932657adaee7daf4504
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-10-04 05:56:27 +0530

    Update 11. Container With Most Water.cpp

--- ARRAY/IMP. QUESTION/11. Container With Most Water.cpp
+++ ARRAY/IMP. QUESTION/11. Container With Most Water.cpp
@@ -2,6 +2,11 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
 
+      // koi bhi do point le skte hai toh farthest left, right pointer lena hai
+      // to calculate how much water it can store use the same logic as trapping rain water
+      // take min of both left and right and * with (j-i) which gives us width
+      // if(left < or even equal to right) the i++ because we may find taller building in right side
+
       int maxCapacity = 0;
       
       int low = 0, high = height.size()-1;
@@ -20,4 +25,5 @@ public:
       return maxCapacity;
         
     }
+
 };
