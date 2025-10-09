e69ca80aaed2b9b5dd0e7564a096b657ccd028af
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-10-09 23:48:44 +0530

    Ques Link Added For  Find the Minimum Amount of Time to Brew Potions.cpp

--- ARRAY/IMP. QUESTION/3494. Find the Minimum Amount of Time to Brew Potions.cpp
+++ ARRAY/IMP. QUESTION/3494. Find the Minimum Amount of Time to Brew Potions.cpp
@@ -1,3 +1,5 @@
+//Ques Link - https://leetcode.com/problems/find-the-minimum-amount-of-time-to-brew-potions/description/?envType=daily-question&envId=2025-10-09
+
 class Solution {
 public:
 
@@ -37,4 +39,5 @@ public:
 
         return time[n-1];
     }
+
 };
