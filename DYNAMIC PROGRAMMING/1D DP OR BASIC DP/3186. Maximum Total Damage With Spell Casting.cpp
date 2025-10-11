ac4b3743b36734c469a0a8e698fd44da9cc17a23
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-10-11 19:13:03 +0530

    Add files via upload

---
+++ DYNAMIC PROGRAMMING/1D DP OR BASIC DP/3186. Maximum Total Damage With Spell Casting.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    typedef long long ll;
+
+    ll findMaxPower(int i, int n, vector<int>&power, vector<ll>&dp, unordered_map<ll,ll>&mpp){
+        
+        if(i >= n) return 0;
+        if(dp[i] != -1) return dp[i];
+
+
+        int nextElement = lower_bound(power.begin() +  i + 1, power.end(), power[i] + 3) - power.begin();
+
+        ll pick = (power[i] * mpp[power[i]] )+ findMaxPower(nextElement,n,power,dp,mpp);
+        ll notPick = findMaxPower(i+1,n,power,dp,mpp);
+
+        return dp[i] = max(pick,notPick);
+    }
+
+    long long maximumTotalDamage(vector<int>& power) {
+
+        unordered_map<ll,ll> mpp;
+        for(auto &x : power) mpp[x]++;
+        sort(power.begin(),power.end());
+        vector<ll> dp(power.size(),-1);
+
+        return findMaxPower(0,power.size(),power,dp,mpp);
+    }
+};
