ce46b3d67e4ba82a0058d4503838543713db394f
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-10-13 06:52:22 +0530

    Add files via upload

---
+++ STRINGS/2273. Find Resultant Array After Removing Anagrams.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+}
+class Solution {
+public:
+
+    bool isAnagram(string &a, string &b){
+
+        unordered_map<char,int> mpp;
+
+        for(auto x : a){
+
+            mpp[x]++;
+        }
+
+        for(auto x : b){
+
+           mpp[x]--;
+           if(mpp[x] == 0) mpp.erase(x);
+        }
+
+        return mpp.empty();
+    }
+
+    vector<string> removeAnagrams(vector<string>& words) {
+        
+        vector<string> ans;
+        deque<string> dq;
+
+        for(auto &x : words){
+
+            if(dq.empty()) dq.push_back(x);
+
+            else{
+
+                if(!dq.empty() && isAnagram(dq.back(),x)){
+
+                    continue;
+
+                }
+
+                else dq.push_back(x);
+            }
+
+        }
+
+        for(auto &x : dq){
+
+            ans.push_back(x);
+        }
+        
+        return ans;
+    }
+};
