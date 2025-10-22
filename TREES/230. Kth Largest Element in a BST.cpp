abff2c4fc3e9ea84a4e792c8beaf848e4b6453e1
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-10-22 09:57:21 +0530

    Add files via upload

---
+++ TREES/230. Kth Largest Element in a BST.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void findElement(TreeNode* root, int &n, int k, int &ans){
+
+        if(!root || ans != -1) return;
+
+        findElement(root -> left, n, k,ans);
+        n++;
+
+        if(n == k) ans = root -> val;
+
+        findElement(root -> right,n,k,ans);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        
+        if(!root) return -1;
+
+        int ans = -1;
+        int n = 0;
+
+        findElement(root,n,k,ans);
+
+        return ans;
+    }
+};
