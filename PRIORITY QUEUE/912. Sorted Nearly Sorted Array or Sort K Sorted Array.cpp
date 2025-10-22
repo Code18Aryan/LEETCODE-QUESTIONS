3dfb494cd2cf8d2135dc82397ff8c83e2c689573
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-10-22 09:59:03 +0530

    Add files via upload

---
+++ PRIORITY QUEUE/912. Sorted Nearly Sorted Array or Sort K Sorted Array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    void nearlySorted(vector<int>& arr, int k) {
+       
+       priority_queue<int,vector<int>,greater<int>> pq;
+       int index = 0;
+       
+       for(int i=0; i<arr.size(); i++){
+           
+           pq.push(arr[i]);
+           
+           if(pq.size() == k+1){
+               
+               arr[index++] = pq.top();
+               pq.pop();
+           }
+       }
+       
+       while(!pq.empty()){
+           
+             arr[index++] = pq.top();
+             pq.pop();           
+       }
+    }
+};
