88abcc5c3ff4432bcc06499f91777ecb3109113d
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-10-23 13:29:47 +0530

    Refactor hasSameDigits function in Solution class

--- STRINGS/3461. Check If Digits Are Equal in String After Operations I.cpp
+++ STRINGS/3461. Check If Digits Are Equal in String After Operations I.cpp
@@ -1,3 +1,5 @@
+//Ques Link - https://leetcode.com/problems/check-if-digits-are-equal-in-string-after-operations-i/description/?envType=daily-question&envId=2025-10-23
+
 class Solution {
 public:
     bool hasSameDigits(string s) {
@@ -22,4 +24,5 @@ public:
 
         return s[0] == s[1];
     }
+
 };
