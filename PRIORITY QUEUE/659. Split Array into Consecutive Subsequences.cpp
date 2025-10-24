e35d0f832af775431ccde66182a43ff023b2c5ed
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-10-24 23:48:16 +0530

    Add files via upload

---
+++ PRIORITY QUEUE/659. Split Array into Consecutive Subsequences.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    typedef pair<int,int> p;
+
+    bool isPossible(vector<int>& nums) {
+        
+        int k = 3;
+        priority_queue<p,vector<p>,greater<p>> pq;
+
+        for(auto &x : nums){
+
+            while(!pq.empty() && pq.top().first + 1 < x){
+
+                if(pq.top().second < k) return 0;
+                pq.pop();
+            }
+
+            if(!pq.empty() && pq.top().first + 1 == x){
+
+                auto it = pq.top();
+                pq.pop();
+
+                pq.push({x,it.second+1});
+            }
+
+            else pq.push({x,1});
+        }
+
+        while(!pq.empty()){
+
+            if(pq.top().second < k) return 0;
+
+            pq.pop();
+        }
+
+        return 1;
+    }
+};
