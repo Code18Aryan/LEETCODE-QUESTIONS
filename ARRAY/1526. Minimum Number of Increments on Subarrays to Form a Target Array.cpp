1df281dab98d3be8c90ce7b3fa08184d7924f7f6
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-10-30 14:52:33 +0530

    Clean up code and formatting in solution class
    Removed commented question link and fixed formatting.

--- ARRAY/1526. Minimum Number of Increments on Subarrays to Form a Target Array.cpp
+++ ARRAY/1526. Minimum Number of Increments on Subarrays to Form a Target Array.cpp
@@ -1,3 +1,5 @@
+// Ques Link - https://leetcode.com/problems/minimum-number-of-increments-on-subarrays-to-form-a-target-array/description/?envType=daily-question&envId=2025-10-30
+
 class Solution {
 public:
     int minNumberOperations(vector<int>& target) {
@@ -11,4 +13,5 @@ public:
 
         return minIncrement;
     }
+
 };
