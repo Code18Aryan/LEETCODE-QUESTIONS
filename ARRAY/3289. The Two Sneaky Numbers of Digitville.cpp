d7947874a455a261a5dac3f880e51cc9c375a9b8
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-10-31 18:10:20 +0530

    Update 3289. The Two Sneaky Numbers of Digitville.cpp

--- ARRAY/3289. The Two Sneaky Numbers of Digitville.cpp
+++ ARRAY/3289. The Two Sneaky Numbers of Digitville.cpp
@@ -1,3 +1,4 @@
+// Ques Link - https://leetcode.com/problems/the-two-sneaky-numbers-of-digitville/description/?envType=daily-question&envId=2025-10-31
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
@@ -15,4 +16,5 @@ public:
         return ans;
 
     }
+
 };
