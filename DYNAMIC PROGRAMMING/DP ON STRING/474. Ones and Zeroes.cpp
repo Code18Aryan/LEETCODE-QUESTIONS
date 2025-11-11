cab1630fbb4967f112c4607cdd8aeed998cba992
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-11-11 20:06:25 +0530

    Add files via upload

---
+++ DYNAMIC PROGRAMMING/DP ON STRING/474. Ones and Zeroes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findMaxCount(int index, int size, vector<string>&str, int m, int n, vector<vector<vector<int>>>&dp){
+
+        if(index >= size) return 0;
+        if(dp[index][m][n] != -1) return dp[index][m][n];
+
+        int zero = count(str[index].begin(),str[index].end(),'0');
+        int one = str[index].size() - zero;
+
+        int take = 0;
+        if (m >= zero && n >= one){
+            take = 1 + findMaxCount(index + 1, size, str, m - zero, n - one, dp);
+        }
+        int notTake = findMaxCount(index + 1, size, str, m,n,dp);
+
+        return dp[index][m][n] = max(take,notTake);
+    }
+
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        
+        int size= strs.size();
+        vector<vector<vector<int>>> dp(size+1, vector<vector<int>> (m+1, vector<int>(n+1,-1)));
+
+        return findMaxCount(0,size,strs,m,n,dp);
+    }
+};
