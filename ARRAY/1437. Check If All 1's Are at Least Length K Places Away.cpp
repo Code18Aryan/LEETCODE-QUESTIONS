35835c0d59af9c0255d7c13f46d39c104bd8ef6f
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-11-17 15:33:15 +0530

    Add files via upload

---
+++ ARRAY/1437. Check If All 1's Are at Least Length K Places Away.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        int j = 0;
+
+        for(int i=0; i<n; i++){
+
+            if(nums[i] == 1){
+
+                j = i;
+                break;
+            }
+        }
+
+        for(int i=j+1; i<n; i++){
+
+            if(nums[i] == 0) continue;
+
+            else{
+
+                int length = i-j-1;
+            
+                if(length < k) return false;
+                j = i;
+            }
+        }
+
+        return true;
+    }
+};
