dba7b7387258344003e406abab470640ec365c40
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-11-22 23:40:55 +0530

    Add files via upload

---
+++ ARRAY/3190. Find Minimum Operations to Make All Elements Divisible by Three.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        
+        int cnt = 0;
+
+        for(auto &x : nums){
+
+            if(x % 3 != 0) cnt++;
+        }
+
+        return cnt;
+    }
+};
