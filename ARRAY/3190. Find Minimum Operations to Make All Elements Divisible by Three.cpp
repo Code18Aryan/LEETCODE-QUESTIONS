92269414ddb2d67b38ee1cc021bd3b3b3bd24c5b
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-11-22 23:41:49 +0530

    Fix formatting and clean up code in Solution class

--- ARRAY/3190. Find Minimum Operations to Make All Elements Divisible by Three.cpp
+++ ARRAY/3190. Find Minimum Operations to Make All Elements Divisible by Three.cpp
@@ -1,3 +1,5 @@
+// QuesLink - https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/description/?envType=daily-question&envId=2025-11-22
+
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
@@ -11,4 +13,5 @@ public:
 
         return cnt;
     }
+
 };
