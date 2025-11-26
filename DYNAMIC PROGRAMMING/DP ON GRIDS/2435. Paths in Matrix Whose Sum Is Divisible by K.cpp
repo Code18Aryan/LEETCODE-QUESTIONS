01f7c7f909efa80ce7e10572e65fd91b243ca728
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-11-26 15:05:18 +0530

    Add files via upload

---
+++ DYNAMIC PROGRAMMING/DP ON GRIDS/2435. Paths in Matrix Whose Sum Is Divisible by K.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    int mod = 1e9 + 7;
+    typedef long long ll;
+
+    int findNumberOfPaths(int i, int j, ll sum, vector<vector<int>>&grid, int k, vector<vector<vector<int>>> &dp){
+
+        if(i < 0 || j < 0) return 0;
+        if(i == 0 && j == 0){
+
+            if((sum + grid[i][j] )% k == 0){
+            
+                return 1;
+            }
+
+            return 0;
+        }
+
+        if(dp[i][j][sum % k] != -1) return dp[i][j][sum % k];
+
+        int up = findNumberOfPaths(i-1,j,sum + grid[i][j],grid,k,dp) % mod;
+        int left = findNumberOfPaths(i,j-1,sum + grid[i][j],grid,k,dp) % mod;
+
+        return dp[i][j][sum % k] = (up + left) % mod;
+    }
+
+    int numberOfPaths(vector<vector<int>>& grid, int k) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+
+        ll sum = 0;
+
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(m+1,vector<int>(k,-1)));
+
+        return findNumberOfPaths(n-1,m-1,sum,grid,k,dp);
+    }
+};
