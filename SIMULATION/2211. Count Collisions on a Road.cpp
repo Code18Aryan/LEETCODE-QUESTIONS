c4fa353e45a5d9edcc0f1639542a0b0b2c1d07b5
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-12-04 23:09:47 +0530

    Add files via upload

---
+++ SIMULATION/2211. Count Collisions on a Road.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int countCollisions(string directions) {
+        
+        stack<char> st;
+
+        int cnt = 0;
+
+        for(auto &x : directions){
+
+            if(x == 'R'){
+
+                st.push(x);
+            }
+
+            else if(x == 'S'){
+
+                while(!st.empty() && st.top() == 'R'){
+
+                    cnt += 1;
+                    st.pop();
+                }
+
+                st.push(x);
+            }
+
+            else{
+
+                if(st.empty()) st.push(x);
+
+                else if(st.top() == 'R'){
+
+                    cnt += 2;
+                    st.pop();
+
+                    while(!st.empty() && st.top() == 'R'){
+
+                        cnt += 1;
+                        st.pop();
+                    }
+
+                    st.push('S');
+                }
+
+                else if(st.top() == 'S'){
+
+                    cnt += 1;
+                    st.push('S');
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
